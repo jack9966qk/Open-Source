1474fac7dee4b14e742ad237d0467d6c7490e3f9
Jack <Jack@Jacks-MacBook-Pro.local>
2017-03-18 15:51:53 +1100

    String extraction from QR code

--- iOS/Open-Source/Open-Source/ViewController.swift
+++ iOS/Open-Source/Open-Source/ViewController.swift
@@ -7,9 +7,32 @@
 //
 
 import UIKit
-import swiftScan
 
-class ViewController: LBXScanViewController {
-
+@IBDesignable class ViewController: LBXScanViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var qrCodes:[LBXScanResult] = []
+    var isScanning = false
+    
+    override func viewDidLoad() {
+        _ = Timer.scheduledTimer(timeInterval: 0.4, target: self, selector: "update", userInfo: nil, repeats: true)
+    }
+    
+    func update() {
+        // print("Hello")
+    }
+    
+    override func handleCodeResult(arrayResult: [LBXScanResult]) {
+        var text:[String] = []
+        for result in arrayResult {
+            print(result.strScanned!)
+            text.append(result.strScanned!)
+        }
+        label.text? = text.joined(separator: "\n")
+        scanObj?.start()
+    }
+    
+    
 }
 
--- iOS/Open-Source/Pods/Target Support Files/Pods-Open-Source/Pods-Open-Source-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Open_Source : NSObject
-@end
-@implementation PodsDummy_Pods_Open_Source
-@end
